649fa674fd7f9d493afdd656408c261d45c1caf9
Segundo Alvarez <segundoav@hotmail.com>
2026-08-03 00:15:09 -0500

    Change auth messages to English

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const isAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.status(401).json({ message: 'No autorizado. Debes iniciar sesión primero.' });
+ res.status(401).json({ message: 'Unauthorized. Please log in first.' });
 };
 
 module.exports = isAuthenticated;
